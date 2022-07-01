fb0bdc92ca2699fb5700df7bd6c1535252479a3f
Pobarul-islam <pobarul3@gmail.com>
2022-07-01 15:04:44 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors')
 require('dotenv').config();
@@ -7,6 +8,18 @@ const app = express();
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USE}:${process.env.DB_PASS}@cluster0.zyufm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('task management app connected');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Surver is Running')
