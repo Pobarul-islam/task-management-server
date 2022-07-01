87c55e770268efdb27580d7e8a4d10422e02f042
Pobarul-islam <pobarul3@gmail.com>
2022-07-02 01:14:57 +0600

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const express = require('express');
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId; 
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -18,6 +20,25 @@ async function run() {
     try {
         await client.connect();
         const taskCollection = client.db('taskManagement').collection('task');
+
+        // get users 
+
+        app.get('/user', async(req, res) => {
+            const query = {};
+            const cursor = taskCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+        app.get('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await taskCollection.findOne(query);
+            res.send(result);
+        })
+
+        // Post user 
+
         app.post('/user', async (req, res) => {
             const newUser = req.body;
             console.log('adding new user', newUser);
@@ -25,9 +46,15 @@ async function run() {
             res.send(result)
         })
 
-        // const user = { name: 'Mahi', email: 'fff@gmail.com', phone: '3928383' };
-        // const result = await taskCollection.insertOne(user);
-        // console.log(`user inserted id:, ${result.insertedId}`)
+        // delete a task
+        
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
 
     finally {
